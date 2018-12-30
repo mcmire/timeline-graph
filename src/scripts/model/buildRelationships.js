e0ec9739971f0d62c78356475b5142bb292b759d
Elliot Winkler <elliot.winkler@gmail.com>
2018-12-30 00:21:12 -0700

    Make the relationship types make more sense

--- src/scripts/model/buildRelationships.js
+++ src/scripts/model/buildRelationships.js
@@ -12,11 +12,9 @@ class Relationship {
       throw new Error(`${this.constructor.type}: No type given`);
     }
 
-    // "subjects" or "children"
     this.from = from;
-    // "objects" or "parents"
     this.to = to;
-    // <from> is a ___ of <to>
+    // <from> is the ___ of <to>
     this.type = type;
   }
 }
@@ -48,7 +46,7 @@ export default function buildNodeGraph(nodes) {
           return lastOccurringNodesByCompany.fetch(source);
         }),
         to: [node],
-        type: "merger",
+        type: "source",
       });
     } else if (event.type === "jointVenture") {
       addRelationship({
@@ -56,7 +54,7 @@ export default function buildNodeGraph(nodes) {
           return lastOccurringNodesByCompany.fetch(contributor);
         }),
         to: [node],
-        type: "jointVenture",
+        type: "source",
       });
     } else if (
       event.type !== "incorporation" &&
@@ -83,65 +81,17 @@ export default function buildNodeGraph(nodes) {
       addRelationship({
         from: [lastOccurringNodesByCompany.fetch(event.data.childCompany)],
         to: [node],
-        type: "subsidiary",
+        type: "acquired",
       });
     } else if (event.data.parentCompany != null) {
       addRelationship({
         from: [lastOccurringNodesByCompany.fetch(event.data.parentCompany)],
         to: [node],
-        type: "subsidiary",
+        type: "parent",
       });
     }
 
     lastOccurringNodesByCompany.set(company, node);
-
-    /*
-    const eventType = node.event.type;
-    const companyId = node.event.company.id;
-    const data = node.event.data;
-
-    function buildRelationshipTo(n) {
-      return { eventType: eventType, node: n };
-    }
-
-    const lastNodeOfSameCompany = lastOccurringNodesByCompanyId[companyId];
-    if (lastNodeOfSameCompany != null) {
-      relationships.push(buildRelationshipTo(lastNodeOfSameCompany));
-    }
-
-    if (data.sources != null) {
-      data.sources.forEach(company => {
-        relationships.push(
-          buildRelationshipTo(lastOccurringNodesByCompanyId[company.id])
-        );
-      });
-    }
-
-    if (data.contributors != null) {
-      data.contributors.forEach(company => {
-        relationships.push(
-          buildRelationshipTo(lastOccurringNodesByCompanyId[company.id])
-        );
-      });
-    }
-
-    if (data.parentCompany != null) {
-      relationships.push(
-        buildRelationshipTo(lastOccurringNodesByCompanyId[data.parentCompany.id])
-      );
-    }
-
-    if (data.oldCompany != null) {
-      relationships.push(
-        buildRelationshipTo(lastOccurringNodesByCompanyId[data.oldCompany.id])
-      );
-    }
-
-    const newNode = node.cloneWith({ relationships });
-    lastOccurringNodesByCompanyId[companyId] = newNode;
-
-    return newNode;
-    */
   });
 
   return relationships;
--- src/scripts/view/addNodeConnectionsTo.js
+++ src/scripts/view/addNodeConnectionsTo.js
@@ -1,8 +1,6 @@
 import * as d3 from "d3";
-//import _ from "lodash";
 import appendCircleTo from "./appendCircleTo";
 import appendLinesTo from "./appendLinesTo";
-//import IncorporationEvent from "../model/IncorporationEvent";
 
 export default function addNodeConnectionsTo(svg, view) {
   svg
@@ -18,32 +16,8 @@ export default function addNodeConnectionsTo(svg, view) {
             { x: rel.to[0].bounds.x1, y: rel.to[0].bounds.halfY },
           ],
         });
-      } else if (rel.type === "subsidiary") {
-        if (rel.from[0].bounds.y2 <= rel.to[0].bounds.y1) {
-          const jut = Math.max(
-            (rel.to[0].bounds.x1 - rel.from[0].bounds.x2),
-            30
-          );
-          const x = rel.to[0].bounds.x1 - jut;
-          appendLinesTo(groupElement, {
-            dashed: true,
-            points: [
-              { x: rel.from[0].bounds.halfX, y: rel.from[0].bounds.y2 },
-              { x: x, y: rel.to[0].bounds.halfY },
-            ],
-          });
-          appendLinesTo(groupElement, {
-            points: [
-              { x: x, y: rel.to[0].bounds.halfY },
-              { x: rel.to[0].bounds.x1, y: rel.to[0].bounds.halfY },
-            ],
-            withArrowhead: true,
-          });
-          appendCircleTo(
-            groupElement,
-            { x: x, y: rel.to[0].bounds.halfY },
-          );
-        } else {
+      } else if (rel.type === "parent" || rel.type === "acquired") {
+        if (rel.from[0].bounds.y1 > rel.to[0].bounds.y1) {
           const jut = Math.max(
             (rel.to[0].bounds.x1 - rel.from[0].bounds.x2),
             30
@@ -67,8 +41,32 @@ export default function addNodeConnectionsTo(svg, view) {
             groupElement,
             { x: x, y: rel.from[0].bounds.halfY },
           );
+        } else {
+          const jut = Math.max(
+            (rel.to[0].bounds.x1 - rel.from[0].bounds.x2),
+            30
+          );
+          const x = rel.to[0].bounds.x1 - jut;
+          appendLinesTo(groupElement, {
+            dashed: true,
+            points: [
+              { x: rel.from[0].bounds.halfX, y: rel.from[0].bounds.y2 },
+              { x: x, y: rel.to[0].bounds.halfY },
+            ],
+          });
+          appendLinesTo(groupElement, {
+            points: [
+              { x: x, y: rel.to[0].bounds.halfY },
+              { x: rel.to[0].bounds.x1, y: rel.to[0].bounds.halfY },
+            ],
+            withArrowhead: true,
+          });
+          appendCircleTo(
+            groupElement,
+            { x: x, y: rel.to[0].bounds.halfY },
+          );
         }
-      } else if (rel.type === "merger" || rel.type === "jointVenture") {
+      } else if (rel.type === "source") {
         rel.from.forEach((source, i) => {
           const args = {
             points: [
@@ -98,142 +96,5 @@ export default function addNodeConnectionsTo(svg, view) {
           });
         });
       }
-
-      /*
-      const jutWidth = rel.from.length > 0 ? 30 : 0;
-
-      const maxXWithJut = _.max(
-        rel.from.map(node => node.bounds.x2 + jutWidth)
-      );
-
-      node.relationships.forEach((relationship, relationshipIndex) => {
-        if (relationship.node.bounds.x1 === node.bounds.x1 && node.event.isHidden) {
-          const y = (
-            node.bounds.y1 < relationship.node.bounds.y1 ?
-              relationship.node.bounds.y1 :
-              relationship.node.bounds.y2
-          );
-          appendLinesTo(groupElement, {
-            dashed: true,
-            points: [
-              {
-                x: node.bounds.x1,
-                y: node.bounds.halfY,
-              },
-              {
-                x: relationship.node.bounds.halfX,
-                y: y,
-              },
-            ],
-            withArrowhead: true,
-          });
-        } else if (relationship.node.bounds.x2 > node.bounds.x1) {
-          if ((node.bounds.x2 - (relationship.node.bounds.x2 + jutWidth)) >= 10) {
-            appendLinesTo(groupElement, {
-              dashed: (node.event instanceof IncorporationEvent),
-              points: [
-                {
-                  x: relationship.node.bounds.x2,
-                  y: relationship.node.bounds.halfY,
-                },
-                {
-                  x: maxXWithJut,
-                  y: relationship.node.bounds.halfY,
-                },
-                {
-                  x: maxXWithJut,
-                  y: (
-                    relationship.node.bounds.y1 > node.bounds.y1 ?
-                      node.bounds.y2 :
-                      node.bounds.y1
-                  ),
-                },
-              ],
-              withArrowhead: (
-                relationship.node.bounds.y1 > node.bounds.y1 ? "up" : "down"
-              ),
-            });
-            appendCircleTo(groupElement, {
-              x: maxXWithJut,
-              y: relationship.node.bounds.halfY,
-            });
-          } else {
-            appendLinesTo(groupElement, {
-              dashed: (node.event instanceof IncorporationEvent),
-              points: [
-                {
-                  x: relationship.node.bounds.x2,
-                  y: relationship.node.bounds.halfY,
-                },
-                {
-                  x: node.bounds.x2 - ((node.bounds.x2 - relationship.node.bounds.x2) / 2),
-                  y: relationship.node.bounds.halfY,
-                },
-                {
-                  x: node.bounds.x2 - ((node.bounds.x2 - relationship.node.bounds.x2) / 2),
-                  y: (
-                    relationship.node.bounds.y1 > node.bounds.y1 ?
-                      node.bounds.y2 :
-                      node.bounds.y1
-                  ),
-                },
-              ],
-              withArrowhead: (
-                relationship.node.bounds.y1 > node.bounds.y1 ? "up" : "down"
-              ),
-            });
-            appendCircleTo(groupElement, {
-              x: node.bounds.x2 - ((node.bounds.x2 - relationship.node.bounds.x2) / 2),
-              y: relationship.node.bounds.halfY,
-            });
-          }
-        } else if (node.event.isHidden) {
-          appendCircleTo(groupElement, {
-            x: node.bounds.x1,
-            y: node.bounds.halfY,
-          });
-          appendLinesTo(groupElement, {
-            points: [
-              { x: node.bounds.x1, y: node.bounds.halfY },
-              { x: relationship.node.bounds.x2, y: relationship.node.bounds.halfY },
-            ],
-          });
-        } else if ((node.bounds.x1 - jutWidth) < relationship.node.bounds.x2) {
-          const points = [
-            { x: relationship.node.bounds.x2, y: relationship.node.bounds.halfY },
-            { x: maxXWithJut, y: relationship.node.bounds.halfY },
-            { x: maxXWithJut, y: node.bounds.y1 },
-          ];
-
-          appendLinesTo(groupElement, { points, withArrowhead: true });
-          appendCircleTo(groupElement, {
-            x: maxXWithJut,
-            y: relationship.node.bounds.halfY,
-          });
-        } else {
-          const points = [
-            { x: relationship.node.bounds.x2, y: relationship.node.bounds.halfY },
-            { x: node.bounds.x1 - jutWidth, y: relationship.node.bounds.halfY },
-            { x: node.bounds.x1 - jutWidth, y: node.bounds.halfY },
-          ];
-          const isLastParent =
-            relationshipIndex === node.relationships.length - 1;
-
-          let withArrowhead = false;
-          if (isLastParent) {
-            points.push({ x: node.bounds.x1, y: node.bounds.halfY });
-            withArrowhead = "right";
-          }
-          appendLinesTo(groupElement, { points, withArrowhead });
-
-          if (node.relationships.length > 1) {
-            appendCircleTo(groupElement, {
-              x: node.bounds.x1 - jutWidth,
-              y: relationship.node.bounds.halfY,
-            });
-          }
-        }
-      });
-      */
     });
 }
